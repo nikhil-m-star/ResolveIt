0a4acbdad4c83b932ff7ffd5fccf5e103d0b6c2d
Nikhil M <nikhilm9110@gmail.com>
2026-04-17 19:19:16 +0530

    UI tweak: colorful map and adjusted top spacing
    - Swapped CartoDB dark tiles in IssueMap to standard OpenStreetMap tiles
    - Removed CSS desaturation/dark filters from leaflet-tile-pane
    - Reduced global top padding in Layout.jsx (from pt-32/pt-48 to pt-24/pt-32)
      to push content slightly higher up on the screen.

--- client/src/components/issues/IssueMap.jsx
+++ client/src/components/issues/IssueMap.jsx
@@ -74,8 +74,8 @@ export function IssueMap({ issues, userLocation }) {
         zoomControl={false}
       >
         <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
-          url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
         {issuesWithCoords.map((issue) => (
           <Marker 
