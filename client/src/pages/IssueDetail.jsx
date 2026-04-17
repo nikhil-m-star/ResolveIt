72eb2aab322f78800292bdedcbdc88c79dd2ff48
Nikhil M <nikhilm9110@gmail.com>
2026-04-17 17:51:14 +0530

    Fix syntax error in IssueDetail.jsx evidence gallery to resolve build failure

--- client/src/pages/IssueDetail.jsx
+++ client/src/pages/IssueDetail.jsx
@@ -307,6 +307,11 @@ export function IssueDetail() {
                      </div>
                    </div>
                  </a>
+               ))}
+             </div>
+          </div>
+        )}
+
         {/* Diagnostic Timeline & Dialogue */}
         <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
            
